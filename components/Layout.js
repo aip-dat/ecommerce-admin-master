4b878f327adf4d8f6eed180e6d93908d60ef63b0
My Name <tkdudat2@gmail.com>
2024-05-21 11:52:17 +0700

    deploy

---
+++ components/Layout.js
@@ -0,0 +1,55 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/Nav";
+import { useState } from "react";
+import Logo from "@/components/Logo";
+
+export default function Layout({ children }) {
+  const [showNav, setShowNav] = useState(false);
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-black w-screen h-screen flex items-center justify-center">
+        <button
+          onClick={() => signIn("google")}
+          className="bg-white p-2 px-4 rounded-lg flex items-center"
+        >
+          <img
+            src="https://cdn.iconscout.com/icon/free/png-512/free-google-160-189824.png?f=webp&w=256"
+            height={32}
+            width={32}
+            alt="Google Logo"
+          />
+          <span className="ml-2">Login with Google</span>
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-bgGray min-h-screen ">
+      <div className="block md:hidden flex items-center p-4">
+        <button onClick={() => setShowNav(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <div className="flex grow justify-center mr-6">
+          <Logo />
+        </div>
+      </div>
+      <div className="flex">
+        <Nav show={showNav} />
+        <div className="flex-grow p-4">{children}</div>
+      </div>
+    </div>
+  );
+}
